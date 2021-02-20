dfa0e34da13f6b771ab9e00c45559443cb7e9e18
proraj9088 <rp8735685@gmail.com>
2021-02-21 02:41:45 +0530

    users created and saved in database
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 
 // User model
-const Use = require('../models/User')
+const User = require('../models/User')
 
  // Login Page
 router.get('/login',(req,res) => res.render('login'))
@@ -61,8 +61,20 @@ router.post('/register',(req,res) =>{
                 email,
                 password
             });
-            console.log(newUser)
-            res.send('hello')
+          // Hash Password 
+          bcrypt.genSalt(10,(err,salt) =>
+           bcrypt.hash(newUser.password,salt,(err,hash) =>{
+                if(err) throw err;
+            // Set password to hash
+                newUser.password = hash;
+
+               // Save User
+               newUser.save() 
+                .then( user => {
+                    res.redirect('users/login');
+                })
+                .catch( err => console.log(err));
+          }))
         }
       });
   }
