081db5af47d6199d3fcffce94d98af98e41ef399
proraj9088 <rp8735685@gmail.com>
2021-02-21 22:24:59 +0530

    login stuff done
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose')
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 
 
 const app = express();
-// DB config
 
+// Passport config
+require('./config/passport')(passport);
+
+// DB config
 const db = require('./config/keys').MongoURI;
 
 // Connect to Mongo
@@ -24,21 +28,27 @@ app.set('view engine', 'ejs');
 
 // Bodyparser
 app.use(express.urlencoded({extended: false}))
+// Express Session
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
-})) 
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect flash
 app.use(flash());
 
 //Global vars
-app.use((req,res,next) =>{
+app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
-});
+  });
 
 
 // Routes
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
 // User model
 const User = require('../models/User')
@@ -80,6 +81,13 @@ router.post('/register',(req,res) =>{
       });
   }
 })
-
+// Login Handle
+router.post('/login', (req,res,next) =>{
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req,res,next);
+})
 
 module.exports = router
